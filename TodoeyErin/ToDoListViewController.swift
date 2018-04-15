4812d66741901af65ce1efb350b705e0bf566911
Erin Krentz <emk@Erins-MacBook-Pro-2.local>
2018-04-15 15:03:57 -0400

    Set up TodoeyErin TVC

---
+++ TodoeyErin/ToDoListViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ToDoListViewController.swift
+//  TodoeyErin
+//
+//  Created by Erin Krentz on 4/15/18.
+//  Copyright © 2018 Erin Krentz. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemsArray = ["Finish Tutorial", "Storyboard", "Select MVP"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemsCell", for: indexPath)
+        let textString = itemsArray[indexPath.row]
+        
+        cell.textLabel?.text = textString
+         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
